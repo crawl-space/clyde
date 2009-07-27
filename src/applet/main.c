8562a15b6e88701e763b552562de42e24ce4c696
James Bowes <jbowes@repl.ca>
2009-07-26 20:11:58 -0400

    watch js0 and print on events

--- src/applet/main.c
+++ src/applet/main.c
@@ -1,10 +1,42 @@
-#include <stdbool.h>
+#include <stdio.h>
+
+#include <linux/joystick.h>
+
 #include <glib.h>
-	
+
+GIOChannel *
+get_joystick (void)
+{
+	int fd = open ("/dev/input/js0");
+	GIOChannel *js = g_io_channel_unix_new (fd);
+
+	return js;
+}
+
+gboolean
+js_data_available (GIOChannel *js, GIOCondition cond, gpointer *data)
+{
+	struct js_event event;
+
+	ssize_t len = read (g_io_channel_unix_get_fd (js), &event,
+			    sizeof (event));
+
+	if (len == sizeof (event)) {
+		printf ("data :: v - %d t - %d n - %d\n", event.value,
+			event.type, event.number);
+	}
+
+	return TRUE;
+}
+
 int
 main (void)
 {
-	GMainLoop *loop = g_main_new (true);
+	GMainLoop *loop = g_main_new (TRUE);
+
+	GIOChannel *js = get_joystick ();
+
+	g_io_add_watch (js, G_IO_IN, (GIOFunc) js_data_available, NULL);
 	g_main_run (loop);
 
 	return 0;
